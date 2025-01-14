d5ca76dfaff3c1cf54bd2c0a64f0ac8d844ed9e8
Daniel wesey <99768882+Biggerwad@users.noreply.github.com>
2025-01-13 20:36:45 -0600

    Request form submission status update (#5)
    * readme changes (#1)
    
    This was from a learning instance
    
    * Comment
    
    * User request form now has a redirect button to request ride again
    
    * Request creation time, and status description UI are now visible
    
    * I fixed Operator form flicker, form status is now descriptive, added loader to member form to handle state management. I made the UI better, having a smooth transition from one screen to another with the help of a 2 second loader, the operator modification page is now responsive, I initiated a prop in List component named total, to calculate the total number of members coming from each geolocation, so it can be displayed on operators dashboard
    
    * I updated the response status conditional to include data.ok
    
    ---------
    
    Co-authored-by: oyerindedavid <oyerindedavid@gmail.com>

--- src/pages/userRequestPage.js
+++ src/pages/userRequestPage.js
@@ -3,8 +3,6 @@ import { getFormStatus } from '../hooks/https';
 import { useJsApiLoader, StandaloneSearchBox } from '@react-google-maps/api'
 import { useNavigate } from 'react-router-dom';
 import Loader from '../components/Loader';
-
-
 const logo = process.env.MY_PUBLIC_URL + '/ppplogo.jpg';
 const API_KEY = process.env.REACT_APP_API_KEY;
 
@@ -78,16 +76,17 @@ function UserRequestPage() {
 
             const data = await response.json();
 
-            if (response) {
+            if (data.ok) {
                 // Handle success (e.g., show success message)
+                console.log(data);
                 console.log('Form submitted successfully:', data);
                 setShowForm(!showForm);
             } else {
-                setShowForm(!showForm);
                 // Handle error (e.g., show error message)
                 console.error('Error submitting form:', data);
                 console.log(data.message);
-                alert("Unable to submit your request, please ensure you provide accurate information.");
+
+                alert("Unable to submit your request, please ensure this is a new request and you are providing accurate information.");
             }
         } catch (error) {
             console.error('Error sending form data:', error);
@@ -166,10 +165,6 @@ function UserRequestPage() {
     function errors(err) {
         console.warn(`ERROR(${err.code}): ${err.message}`);
     }
-
-
-
-
 
     return (
         <div>
